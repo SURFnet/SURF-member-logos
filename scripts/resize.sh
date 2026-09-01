3a4aa337eedb3b932e9f605555e2712a7dabe433
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2026-09-01 17:02:23 +0200

    Update resizer script

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -11,7 +11,7 @@ SIZE=$1
 
 if [ -z "$SIZE" ]; then
     echo "This script reduces the colorspace, resizes and recompresses all images" >&2
-    echo "All new images will be written to the corrsponding directory; so size=200 will create 200x200/" >&2
+    echo "All new images will be written to the corresponding directory; so size=200 will create 200x200/*" >&2
     echo "Syntax: $0 <size>"
     exit 1
 fi
@@ -21,40 +21,67 @@ if ! [[ "$SIZE" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
-if [ "$SIZE" -lt 64 ] || [ "$SIZE" -gt 1024 ]; then
+if (( "$SIZE" < 64 )) || (( "$SIZE" > 1024 )); then
     echo "Error: SIZE must be between 64 and 1024" >&2
     exit 1
 fi
 
 
 function process() {
-    img=$1
-    destdir=$2
+    local img=$1
+    local size=$2
+    local destdir=$3
+
+    local dest="${destdir}/${img}"
+
+    if [ -e "$dest" ] && [ "$dest" -nt "$img" ]
+    then
+        #echo "Skipping ${img}..."
+        return 0
+    fi
 
     echo "Processing ${img}..."
 
-    # two-step encoding process because we need to find the number of colors to make the palette as small as possible
-    tmp="${destdir}/$(basename "${img}" ".png").step1.png"
-    dest="${destdir}/${img}"
+    # basic checks
+    read -r orig_width orig_height < <(
+        magick -quiet "$img" -format '%[width] %[height]\n' info:
+    )
 
-    magick "$img" \
-        -resize "${SIZE}x${SIZE}!" \
-        -extent "${SIZE}x${SIZE}" \
+    if (( orig_width != orig_height ))
+    then
+        echo "  - skipping: image is not square (${orig_width}x${orig_height})"
+        return 1
+    fi
+    if (( orig_width < size ))
+    then
+        echo "  - skipping: image is smaller than ${size}px (${orig_width}x${orig_height})"
+        return 1
+    fi
+
+
+    # two-step encoding process because we need to find the number of colors to make the palette as small as possible
+    local tmp="${destdir}/$(basename "${img}" ".png").step1.png"
+
+    magick -quiet \
+        "$img" \
+        -strip \
+        -resize "${size}x${size}!" \
+        -extent "${size}x${size}" \
         -background none \
         -gravity center \
         -colors 255 \
         +dither \
         "$tmp"
 
-    ncolors=$(magick "$tmp" -format "%k" info:)
+    local ncolors=$(magick "$tmp" -format "%[colors]" info:)
 
+    local depth
     if   [ "$ncolors" -le 4 ]; then depth=2
     elif [ "$ncolors" -le 16 ]; then depth=4
     else depth=8
     fi
 
     magick "$tmp" \
-        -strip \
         -define png:bit-depth=$depth \
         -define png:color-type=3 \
         -define png:compression-level=9 \
@@ -63,24 +90,34 @@ function process() {
         "$dest"
 
     # Check if original is smaller than target
-    tmp_size=$(stat -f%z "$tmp" 2>/dev/null || stat -c%s "$tmp" 2>/dev/null)
-    dest_size=$(stat -f%z "$dest" 2>/dev/null || stat -c%s "$dest" 2>/dev/null)
+    local tmp_size=$(stat -f%z "$tmp" 2>/dev/null || stat -c%s "$tmp" 2>/dev/null)
+    local dest_size=$(stat -f%z "$dest" 2>/dev/null || stat -c%s "$dest" 2>/dev/null)
     if [ "$tmp_size" -le "$dest_size" ]; then
-        echo "Tmp ${tmp} is smallest (${tmp_size} bytes), using tmp"
+        #echo "  - intermediate image ${tmp} is smallest (${tmp_size} vs ${dest_size} bytes), using intermediate"
         cp "$tmp" "$dest"
     fi
     rm -f "$tmp"
 
+    return 0
 }
 
 
 SUBDIR="${SIZE}x${SIZE}"
-rm -rf "$SUBDIR"
 mkdir -p "$SUBDIR"
 
+errors=0
 for image in *.png
 do
-    process "$image" "$SUBDIR"
+    if ! process "$image" "$SIZE" "$SUBDIR"
+    then
+        errors=$((errors + 1))
+    fi
 done
+
+if (( errors > 0 ))
+then
+    echo "Encountered ${errors} errors while processing"
+    exit 1
+fi
 
 exit 0
